ce5ae215a31a973b90fc607bf5c738dcd16f3b51
exakoss <daniel@omni-x.io>
2024-03-24 11:23:43 -0400

    feat: add authors and proper metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,9 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   // without a title, warpcast won't validate your frame
-  title: "frames.js starter",
-  description: "...",
+  title: "Superchain ONFT Frame",
+  description: "Superchain ONFT Frame is a ONFT mint on Base and Zora chains on Farcaster. Powered by LayerZero and frames.js this frame enables a creator first omnichain NFT experience directly in your fee~!",
+  authors: [{name: "boobavelli", url: "https://warpcast.com/boobavelli"}, {name: 'exakoss', url: 'https://github.com/exakoss'}]
 };
 
 export default function RootLayout({
